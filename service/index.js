535f188ae0bf158dde94842daf52056961d94240
Jinho Lee <jinholee@byu.edu>
2026-08-05 19:13:44 -0600

    Fix duplicate app.listen

--- service/index.js
+++ service/index.js
@@ -135,10 +135,6 @@ function setAuthCookie(res, authToken) {
   });
 }
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
 const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
